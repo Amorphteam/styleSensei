2cc6cee666f2655749650c5119dd0c8c4f03ade2
ali johar <johar.ali1@gmail.com>
2023-10-20 11:15:22 +0330

    Add error page

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -16,11 +16,24 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   final PageController _pageController = PageController(initialPage: 0);
   final List<Widget> splashPages = [
-    SplashPageWithPattern(Colors.white, "assets/images/splash0.png", "Never stop looking for "),
-    SplashPage(Colors.white, "assets/images/splash1.png", "Getting to know you", "This journey is all about YOU. in this Process we will determineyour fashion goals by learning about you.", false),
-    SplashPage(Colors.white, "assets/images/splash2.png", "Discover your personal style", "We can help you develop your personal look, infuse your closet with pieces that feel like YOU and help finding the perfect Outfits for various occasions, daily wear, or special work presentations.", true),
-    LoadingPage(Colors.white, "assets/images/loading.png", "We are tailoring your space to YOUR style"),
-
+    SplashPageWithPattern(
+        Colors.white, "assets/images/splash0.png", "Never stop looking for "),
+    SplashPage(
+        Colors.white,
+        "assets/images/splash1.png",
+        "Getting to know you",
+        "This journey is all about YOU. in this Process we will determineyour fashion goals by learning about you.",
+        false),
+    SplashPage(
+        Colors.white,
+        "assets/images/splash2.png",
+        "Discover your personal style",
+        "We can help you develop your personal look, infuse your closet with pieces that feel like YOU and help finding the perfect Outfits for various occasions, daily wear, or special work presentations.",
+        true),
+    LoadingPage(Colors.white, "assets/images/loading.png",
+        "We are tailoring your space to YOUR style"),
+    ErrorPage(Colors.white, "assets/images/error.png", "Internal Server Error",
+        "Oops! Something went wrong on our end."),
   ];
 
   double pageIndex = 0;
@@ -49,7 +62,8 @@ class _SplashScreenState extends State<SplashScreen> {
                 position: pageIndex.toInt(),
                 decorator: DotsDecorator(
                   size: const Size.square(9.0),
-                  color: Colors.grey, // Color of inactive dots
+                  color: Colors.grey,
+                  // Color of inactive dots
                   activeSize: const Size(9.0, 9.0),
                   activeShape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5.0),
@@ -72,7 +86,8 @@ class SplashPage extends StatelessWidget {
   final String des;
   final bool isTheLastSplash;
 
-  SplashPage(this.backgroundColor, this.imagePath, this.title, this.des, this.isTheLastSplash);
+  SplashPage(this.backgroundColor, this.imagePath, this.title, this.des,
+      this.isTheLastSplash);
 
   @override
   Widget build(BuildContext context) {
@@ -143,7 +158,8 @@ class SplashPage extends StatelessWidget {
             right: 40,
             child: GestureDetector(
               onTap: () {
-                final homeCubit = HomeCubit(); // Create an instance of HomeCubit
+                final homeCubit =
+                    HomeCubit(); // Create an instance of HomeCubit
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -161,10 +177,12 @@ class SplashPage extends StatelessWidget {
     );
   }
 }
+
 class SplashPageWithPattern extends StatelessWidget {
   final Color backgroundColor;
   final String imagePath;
   final String title;
+
   SplashPageWithPattern(this.backgroundColor, this.imagePath, this.title);
 
   @override
@@ -192,47 +210,43 @@ class SplashPageWithPattern extends StatelessWidget {
               ),
               SizedBox(height: 20),
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40), // Adjust the padding as needed
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                // Adjust the padding as needed
                 child: Text(
                   "ــــ",
-                  style: TextStyle(
-                    color: Colors.black12
-                  ),
+                  style: TextStyle(color: Colors.black12),
                 ),
               ),
               SizedBox(height: 20),
-
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40), // Adjust the padding as needed
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: title,
-                        style: TextStyle(
-                          color: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  // Adjust the padding as needed
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: title,
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: 'I',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-
+                        TextSpan(
+                          text: 'I',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: 'nspiration',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
+                        TextSpan(
+                          text: 'nspiration',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                )
-
-              ),
+                      ],
+                    ),
+                  )),
               Expanded(
                 child: Image.asset(
                   imagePath,
@@ -246,6 +260,7 @@ class SplashPageWithPattern extends StatelessWidget {
     );
   }
 }
+
 class LoadingPage extends StatelessWidget {
   final Color backgroundColor;
   final String imagePath;
@@ -301,22 +316,84 @@ class LoadingPage extends StatelessWidget {
             ],
           ),
         ),
-          Positioned(
-            bottom: 20,
-            left: 40,
-            child: GestureDetector(
-              onTap: () {
-
-              },
-              child: SvgPicture.asset("assets/images/arrow_left.svg"),
-            ),
+        Positioned(
+          bottom: 20,
+          left: 40,
+          child: GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset("assets/images/arrow_left.svg"),
           ),
+        ),
       ],
     );
   }
 }
 
+class ErrorPage extends StatelessWidget {
+  final Color backgroundColor;
+  final String imagePath;
+  final String title;
+  final String des;
 
+  ErrorPage(this.backgroundColor, this.imagePath, this.title, this.des);
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 40),
+                  child: Image.asset(imagePath, width: 200, height: 200),
+                ),
+                Positioned(
+                  top: 100,
+                  right: 20,
+                  child: Container(
+                    width: 250,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Text(
+                          des,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        SizedBox(height: 80),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: TextButton(
+              onPressed: () {
+                // Add your action or function to be executed when the button is pressed here
+              },
+              child: Text(
+                'Refresh',
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  // Add other text styling properties if needed
+                ),
+              )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
