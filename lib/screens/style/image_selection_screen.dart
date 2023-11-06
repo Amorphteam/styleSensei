27584b30b2c9541e612cd28a1156899c3428214c
ali johar <johar.ali1@gmail.com>
2023-11-06 09:41:36 +0330

    Fix the style selected ui

--- lib/screens/style/image_selection_screen.dart
+++ lib/screens/style/image_selection_screen.dart
@@ -38,107 +38,121 @@ class _ImageSelectionScreenState extends State<ImageSelectionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: ListView(
-        children: [
-          SizedBox(height: 8),
-          Container(
-              margin: EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
+      children: [
+        Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              SizedBox(height: 8),
+              Container(
+                  margin: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Pick 4 or more styles you like',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleLarge
-                            ?.copyWith(fontWeight: FontWeight.bold),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Pick 4 or more styles you like',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'By understanding your preferences, \nwe can offer you tailored recommendations.',
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                        ],
                       ),
-                      Text(
-                        'By understanding your preferences, \nwe can offer you tailored recommendations.',
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
+                      Image.asset(
+                        'assets/images/large_text_logo.png',
+                        width: 12,
+                      )
                     ],
-                  ),
-                  Image.asset(
-                    'assets/images/large_text_logo.png',
-                    width: 12,
-                  )
-                ],
-              )),
-          SizedBox(height: 8),
-          GridView.builder(
-            physics: NeverScrollableScrollPhysics(), // to disable GridView's scrolling
-            shrinkWrap: true, // You won't see infinite size error
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 2,
-              mainAxisSpacing: 2,
-              childAspectRatio: 0.75,
-            ),
-            itemCount: images.length,
-            itemBuilder: (context, index) {
-              bool isSelected = selectedIndexes.contains(index);
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (isSelected) {
-                      selectedIndexes.remove(index);
-                    } else {
-                      selectedIndexes.add(index);
-                    }
-                  });
-                },
-                child: GridTile(
-                  child: Stack(
-                    children: [
-                      Image.asset(images[index].path, fit: BoxFit.cover),
-                      if (!isSelected) // If not selected, show the semi-transparent overlay
-                        Container(color: Colors.black38),
-                    ],
-                  ),
-                  footer: GridTileBar(
-                    title: Container(), // This can be used to push the trailing icon to the right if needed.
-                    trailing: Align(
-                      alignment: Alignment.centerRight,
-                      child: isSelected
-                          ? Icon(Icons.check_circle, color: Colors.red)
-                          : Icon(Icons.radio_button_off, color: Colors.white),
-                    ),
-                  ),
+                  )),
+              SizedBox(height: 8),
+              GridView.builder(
+                physics: NeverScrollableScrollPhysics(), // to disable GridView's scrolling
+                shrinkWrap: true, // You won't see infinite size error
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                  childAspectRatio: 0.75,
                 ),
-              );
-            },
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  bool isSelected = selectedIndexes.contains(index);
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (isSelected) {
+                          selectedIndexes.remove(index);
+                        } else {
+                          selectedIndexes.add(index);
+                        }
+                      });
+                    },
+                    child: GridTile(
+                      child: Stack(
+                        children: [
+                          Image.asset(images[index].path, fit: BoxFit.cover),
+                          if (!isSelected) Container(color: Colors.black38), // Semi-transparent overlay
+                        ],
+                      ),
+                      footer: GridTileBar(
+                        title: Text(''), // Empty text widget can be used to push the icon to the right if needed.
+                        trailing: isSelected
+                            ? Icon(Icons.check_circle, color: Colors.red)
+                            : Icon(Icons.radio_button_off, color: Colors.white),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-          Container(
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          left: 0,
+          child: Container(
+            color: Colors.white,
             padding: EdgeInsets.all(8),
             child: ElevatedButton(
               onPressed: selectedIndexes.length >= 3
                   ? () {
                 printSelectedTags();
-                final homeCubit = HomeCubit(); // Create an instance of HomeCubit
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BlocProvider(
-                      create: (context) => homeCubit,
-                      child: MyHomePage(),
-                    ),
-                  ),
-                );
+                // Your navigation logic here
               }
-                  : null, // Button is disabled if less than 3 items are selected
+                  : null, // Button is disabled if less than 4 items are selected
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return Colors.grey; // Disabled color
+                    }
+                    return selectedIndexes.length < 3 ? Colors.grey : Colors.black; // Enable color changes
+                  },
+                ),
+              ), // Button is disabled if less than 3 items are selected
               child: Text(
-                'Pick ${3 - selectedIndexes.length} more',
+                selectedIndexes.length < 3
+                    ? 'Pick ${3 - selectedIndexes.length} more'
+                    : 'Get Recommendations',
+                style: TextStyle(
+                  color: Colors.white, // Change color conditionally
+                ),
               ),
+
             ),
           ),
-        ],
-      ),
+        ),
+
+      ],
     );
   }
 }
