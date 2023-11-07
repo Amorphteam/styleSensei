4a1d7d52c2c889bca6c2e640eba4dd8adc70708e
ali johar <johar.ali1@gmail.com>
2023-11-07 13:11:19 +0330

    build new app version to fix some issues

--- lib/screens/home_tab/home_screen.dart
+++ lib/screens/home_tab/home_screen.dart
@@ -47,146 +47,148 @@ class _HomeTabState extends State<HomeTab> {
       child: ListView(
         children: [
           SizedBox(height: 8),
-          Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    // Align the text to the right side of the container
+          Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      // Align the text to the right side of the container
 
-                    child: Text(
-                      'Hello there!',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          'assets/images/circle.png',
-                          width: 50,
-                          height: 50,
-                        ),
-                      ),
-                      Expanded(
-                        child: RichText(
-                          text: TextSpan(
-                            style: Theme.of(context).textTheme.titleSmall, // Default text style
-                            children: <TextSpan>[
-                              TextSpan(text: 'It looks like your top choices revolve around '),
-                              TextSpan(
-                                text: '$styleName',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(text: ' attire.'),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-
-                  SizedBox(height: 24.0),
-                  // Placeholder for images, you would use Image.asset or Image.network instead
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: Row(
-                      children: [
-                        // Left image
-                        Transform.rotate(
-                          angle: -0.2,
-                          // Adjust the angle to tilt the card to the left
-                          child: ImageCard(assetName: imageAssetsUrl[0].path),
-                        ),
-                        // Right image
-
-                        // Center image (no rotation)
-                        Transform.rotate(
-                          angle: 0.0,
-                          // Adjust the angle to tilt the card to the right
-                          child: ImageCard(assetName: imageAssetsUrl[1].path),
-                        ),
-                        Transform.rotate(
-                          angle: 0.1,
-                          // Adjust the angle to tilt the card to the right
-                          child: ImageCard(assetName: imageAssetsUrl[2].path),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 24.0),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          'assets/images/cards.png',
-                          width: 40,
-                          height: 40,
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                            'Based on your preferences, you might prefer these styles:',
-                            style: Theme.of(context).textTheme.titleSmall),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16.0),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(), // Disable scrolling for the nested ListView.builder
-                    shrinkWrap: true, // Use this to fit the ListView into the available space
-                    itemCount: styleDescriptions.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(left: 20, right: 20),
-                        child: BulletPoint(text: styleDescriptions[index]),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextButton(
-                    onPressed: () async {
-                      await Future.delayed(Duration(microseconds: 200));
-                      final imageSelectionCubit =
-                          ImageSelectionCubit(); // Create an instance of HomeCubit
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BlocProvider(
-                            create: (context) => imageSelectionCubit,
-                            child: StyleScreen(),
-                          ),
-                        ),
-                      );
-                    },
-                    child: RichText(
-                      text: TextSpan(
+                      child: Text(
+                        'Hello there!',
                         style: Theme.of(context)
                             .textTheme
-                            .labelSmall, // Default text style
-                        children: [
-                          TextSpan(
-                            text:
-                                'If the result doesn\'t match your style, ', // Unstyled part
+                            .titleMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            'assets/images/circle.png',
+                            width: 50,
+                            height: 50,
                           ),
-                          TextSpan(
-                            text: 'Try the test again', // Styled part
-                            style: TextStyle(
-                                color: Colors.red), // Apply the red color
+                        ),
+                        Expanded(
+                          child: RichText(
+                            text: TextSpan(
+                              style: Theme.of(context).textTheme.titleSmall, // Default text style
+                              children: <TextSpan>[
+                                TextSpan(text: 'It looks like your top choices revolve around '),
+                                TextSpan(
+                                  text: '$styleName',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(text: ' attire.'),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+
+                    SizedBox(height: 24.0),
+                    // Placeholder for images, you would use Image.asset or Image.network instead
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                        children: [
+                          // Left image
+                          Transform.rotate(
+                            angle: -0.2,
+                            // Adjust the angle to tilt the card to the left
+                            child: ImageCard(assetName: imageAssetsUrl[0].path),
+                          ),
+                          // Right image
+
+                          // Center image (no rotation)
+                          Transform.rotate(
+                            angle: 0.0,
+                            // Adjust the angle to tilt the card to the right
+                            child: ImageCard(assetName: imageAssetsUrl[1].path),
+                          ),
+                          Transform.rotate(
+                            angle: 0.1,
+                            // Adjust the angle to tilt the card to the right
+                            child: ImageCard(assetName: imageAssetsUrl[2].path),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 24.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            'assets/images/cards.png',
+                            width: 40,
+                            height: 40,
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                              'Based on your preferences, you might prefer these styles:',
+                              style: Theme.of(context).textTheme.titleSmall),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    ListView.builder(
+                      physics: NeverScrollableScrollPhysics(), // Disable scrolling for the nested ListView.builder
+                      shrinkWrap: true, // Use this to fit the ListView into the available space
+                      itemCount: styleDescriptions.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: BulletPoint(text: styleDescriptions[index]),
+                        );
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextButton(
+                      onPressed: () async {
+                        await Future.delayed(Duration(microseconds: 200));
+                        final imageSelectionCubit =
+                            ImageSelectionCubit(); // Create an instance of HomeCubit
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BlocProvider(
+                              create: (context) => imageSelectionCubit,
+                              child: StyleScreen(),
+                            ),
+                          ),
+                        );
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelSmall, // Default text style
+                          children: [
+                            TextSpan(
+                              text:
+                                  'If the result doesn\'t match your style, ', // Unstyled part
+                            ),
+                            TextSpan(
+                              text: 'Try the test again', // Styled part
+                              style: TextStyle(
+                                  color: Colors.red), // Apply the red color
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
